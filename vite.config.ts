293c3d61f18e1919678dd041ed54a6d16bff6754
Dan <partytimeyep@gmail.com>
2024-08-02 14:24:07 +0300

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/subscriber/',
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
   server: {
     proxy: {
       '/api/v1': {
